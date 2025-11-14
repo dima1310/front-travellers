5e1aa0d8ad6d5c0d9e6d55630551466daeb526a0
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-11-14 13:14:59 +0200

    add-story-page змінив файли useAuthStore.tsx та LoginForm.tsx + додав коментарі в код що саме змінив, функціонал працює , але там в мене проблеми з css, я не зможу то виправити

--- src/components/forms/LoginForm/LoginForm.tsx
+++ src/components/forms/LoginForm/LoginForm.tsx
@@ -7,6 +7,9 @@ import { api } from "@/services/api/axiosConfig";
 import AuthForm from "../AuthForm/AuthForm";
 import styles from "./LoginForm.module.css";
 
+// ✅ ДОДАНО — підключаємо Zustand Auth Store
+import { useAuthStore } from "@/store/useAuthStore";
+
 import {
   loginFields,
   loginInitialValues,
@@ -29,11 +32,20 @@ type ApiErrorShape = {
 export default function LoginForm() {
   const router = useRouter();
 
+  // ✅ ДОДАНО — отримуємо login() зі стору
+  const login = useAuthStore((state) => state.login);
+
   const handleLogin = async (values: LoginFormValues) => {
     try {
       const res = await api.post("/auth/login", values);
 
       if (res.status === 200 || res.status === 201) {
+
+        // ⬇️⬇️⬇️ ГОЛОВНА ЗМІНА !!!
+        // ТУТ ми зберігаємо user + token у Zustand
+        // щоб isAuthenticated став true
+        login(res.data.user, res.data.token);
+
         showSuccessToast("Логін успішний");
         router.push("/");
       } else {
