7700246e0cc9d136d90c4f612597962482b6c77b
lilianantw <liliana.ntw@gmail.com>
2025-11-16 00:54:54 +0200

    стили home

--- src/components/home/About/About.tsx
+++ src/components/home/About/About.tsx
@@ -1,33 +1,52 @@
-"use client";
-
-import Image from "next/image";
 import styles from "./About.module.css";
 
 export default function About() {
   return (
-    <section className={styles.about} id="about">
+    <section className={styles.section}>
       <div className={styles.container}>
-        <div className={styles.imageWrapper}>
-          <Image
-            src="/images/about.jpg" // заміни на своє зображення з макету
-            alt="Подорожі навколо світу"
-            width={600}
-            height={480}
-            className={styles.image}
-          />
+
+        {/* LEF BLOCK */}
+        <div className={styles.titleBlock}>
+          <h2 className={styles.title}>
+            Проєкт, створений для тих, <br />
+            хто живе подорожами
+          </h2>
         </div>
 
-        <div className={styles.textBlock}>
-          <h2 className={styles.title}>
-            Проєкт, створений для тих, хто живе подорожами
-          </h2>
-          <p className={styles.description}>
-            Ми віримо, що кожна подорож — це унікальна історія, варта того, щоб
-            нею поділитися. Наша платформа створена, щоб об’єднати людей,
-            закоханих у відкриття нового. Тут ви можете ділитися власним
-            досвідом, знаходити друзів та надихатися на наступні пригоди разом з
-            нами.
-          </p>
+        {/* RIGHT TEXT */}
+        <p className={styles.description}>
+          Ми віримо, що кожна подорож — це унікальна історія,
+          варта того, щоб нею поділилися. Наша платформа створена,
+          щоб об’єднати людей, закоханих у відкриття нового...
+        </p>
+
+        {/* FEATURES */}
+        <div className={styles.features}>
+
+          <div className={styles.featureItem}>
+            <img src="/public/icons/Icon1.svg" className={styles.icon} />
+            <h3 className={styles.featureTitle}>Наша місія</h3>
+            <p className={styles.featureText}>
+              Об’єднувати людей через любов до пригод...
+            </p>
+          </div>
+
+          <div className={styles.featureItem}>
+            <img src="/public/icons/Icon2.svg" className={styles.icon} />
+            <h3 className={styles.featureTitle}>Автентичні історії</h3>
+            <p className={styles.featureText}>
+              Ми цінуємо справжні, непереглянуті враження...
+            </p>
+          </div>
+
+          <div className={styles.featureItem}>
+            <img src="/public/icons/Vector3.svg" className={styles.icon} />
+            <h3 className={styles.featureTitle}>Ваша спільнота</h3>
+            <p className={styles.featureText}>
+              Станьте частиною спільноти, де кожен може бути...
+            </p>
+          </div>
+
         </div>
       </div>
     </section>
